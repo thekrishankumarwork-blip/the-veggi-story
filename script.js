80937b29136305b9964d4a0a5bf84a6622ef8ed9
thekrishankumarwork-blip <thekrishankumarwork@gmail.com>
2026-05-31 06:40:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let orderNumber = 1000;
-
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(button => {
-button.addEventListener("click", () => {
-orderNumber++;
-
-alert(
-"Order Placed Successfully!\n\nOrder Number: TVS" +
-orderNumber
-);
-});
-});
+function orderNow(orderId){
+    alert("Order Placed Successfully!\n\nOrder Number: " + orderId);
+}
